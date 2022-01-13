662ea1a206e52fbfbdf7021eca23999d6d44722b
Shashwat Shriparv <dwivedishashwat@gmail.com>
2022-01-13 16:22:55 +0530

    Checking variable type

--- VariablesStringInt.py
+++ VariablesStringInt.py
@@ -1,5 +1,10 @@
 integerval=10
 stringvalue='Stringvalue'
+floatvalue=10.5
 print(integerval)
 print(stringvalue)
-print(stringvalue +":"+  str(integerval))
+print(stringvalue +":"+  str(integerval)+":"+str(floatvalue))
+print(type(integerval))
+print(type(stringvalue))
+print(type(floatvalue))
+
