e66b06ac876b42108867c285eeac4705fa48a1fa
Shashwat Shriparv <dwivedishashwat@gmail.com>
2022-01-18 22:56:43 +0530

    adding operator file

---
+++ operators.py
@@ -0,0 +1,10 @@
+'''Mathematical Operation '''
+intval=22
+floatval=33.33
+print("Addition:",intval+intval)
+print("Substraction:",intval-floatval)
+print("Division:",intval/floatval)
+print("Multiplication:",intval*floatval)
+print("Bodmas:",intval+floatval-intval/floatval*intval)
+print("Bodmas:",((intval+floatval)-(intval/floatval))*(intval*intval))
+print("Bodmas:",((intval+floatval)-(intval/floatval))*(intval*intval)/floatval)
